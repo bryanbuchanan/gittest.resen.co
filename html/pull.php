db9b22faa7162338c5b04c689c271c0f2904350b
Bryan Buchanan <bryanbuchanan@new-host-5.home>
2013-05-16 13:29:19 -0700

    pull update

--- html/pull.php
+++ html/pull.php
@@ -1,1 +1,10 @@
-<? `git pull` ?>
+<?
+
+if (!empty($_POST['payload'])):
+
+	$output = exec('git pull');
+	mail("bryan@resen.co", "CalvinBecerra.com Push", $output, "From: CalvinBecerra.com <server@calvinbecerra.com>");
+
+endif;
+
+?>
